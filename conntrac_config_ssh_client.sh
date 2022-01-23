bfa8e51ad415223cf44fe71b086d85533809c8b2
Aleksandr Maksimov <coffeeman@yandex-team.ru>
2022-01-24 01:22:05 +0300

    fix shebang

--- conntrac_config_ssh_client.sh
+++ conntrac_config_ssh_client.sh
@@ -1,4 +1,4 @@
-#!/usr/
+#!/usr/bin/bash
 iptables -t mangle -A INPUT -i eth0 -p tcp --dport 22 -j CONNMARK --set-mark 0x2
 iptables -t mangle -A OUTPUT -j CONNMARK --restore-mark
 ip rule add fwmark 0x2/0x2 lookup main
